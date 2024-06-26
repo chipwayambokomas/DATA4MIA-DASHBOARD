fd76b36e3f2b2552557f2d2a8f4469aa3e3692f5
chipwayambokomas <chipwayambokomas@gmail.com>
2024-06-26 14:35:41 +0200

    filter data based on desired coloumn headers

--- src/data/IAR_reader.py
+++ src/data/IAR_reader.py
@@ -1,4 +1,15 @@
 import pandas as pd
 
+desired_columns = ['Organisation', 'Organisational ID', 'Number', 'Model', 'Serial Number', 'Description', 'Material Type', 'Asset Class ID', 'Sector ID', 'Size', 'Size Measurement (unit)', 'Capacity', 'Quantity', 'Utilisation', 'Replacement Equivalent', 'Year Constructed / Purchase Date', 'Supplier Name', 'Criticality Grade ID', 'Condition ID', 'Location X', 'Location Y', 'Custodian', 'Replacement Cost Per Item', 'Purchase Price', 'Actual Deemed Cost', 'Depreciated Replacement Cost', 'Current Replacement Cost', 'Additional Amount', 'EUL in Years', 'Age', 'RUL in Years'] 
+
 data = pd.read_excel("C:/Users/NEW ERA/Documents/DATA4MIA/DATA4MIA-DASHBOARD/data/raw/Copy_of_Emalahleni_Infrastructure_2023.xlsx")
+
+filtered_columns = []
+
+for col in desired_columns:
+    if col in data.columns:
+        filtered_columns.append(col)
+        
+        
+filtered_data = data[filtered_columns]
 
